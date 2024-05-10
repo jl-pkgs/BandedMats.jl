7724372049f2928adcc6bd3a612838299df7466f
Dongdong Kong <kongdd@users.noreply.github.com>
2024-05-10 16:03:11 +0800

    minor update

--- src/LU_solve.jl
+++ src/LU_solve.jl
@@ -1,10 +1,13 @@
 export solve_L, solve_U
 
 function solve_U(U::AbstractMatrix{T}, b::AbstractArray) where {T}
-  n = length(b)
-  x = similar(b)
-  x[n] = b[n] / U[n, n]
+  # Ux = b
+  # [n, m] * [m, 1] = [n,1]
+  n, m = size(U)
+  x = zeros(T, m)
 
+  # x[n] = b[n] / U[n, n]
+  x[m] = b[n] / U[n, m]
   @inbounds for i = n-1:-1:1
     x[i] = b[i] - sum(U[i, i+1:n] .* x[i+1:n])
     x[i] /= U[i, i]
@@ -12,20 +15,6 @@ function solve_U(U::AbstractMatrix{T}, b::AbstractArray) where {T}
   return x
 end
 
-function solve_L(L::AbstractMatrix{T}, b::AbstractArray) where {T}
-  n = length(b)
-  x = similar(b)
-  x[1] = b[1] / L[1, 1]
-
-  @inbounds for i = 2:n
-    x[i] = b[i] - sum(L[i, 1:i-1] .* x[1:i-1])
-    x[i] /= L[i, i]
-  end
-  return x
-end
-
-################################################################################
-## 带状矩阵的版本, kong的数据压缩方式
 function solve_U(BU::BandedMat{T}, b::AbstractArray) where {T}
   # [i, j] => [i, j - i + 1]     # 对于U
   (; q) = BU
@@ -36,11 +25,30 @@ function solve_U(BU::BandedMat{T}, b::AbstractArray) where {T}
 
   @inbounds for i = n-1:-1:1
     # x[i] = b[i] - sum(U[i, i+1:n] .* x[i+1:n])
-    x[i] = b[i] 
-    for k = i+1:min(i+q, n)
+    x[i] = b[i]
+    for k = i+1:min(i + q, n)
       x[i] -= U[i, k-i+1] * x[k]
     end
     x[i] /= U[i, 1]
+  end
+  return x
+end
+
+# 超定齐次线性：也存在求解困难
+# 非齐次方程无法求解
+function solve_L(L::AbstractMatrix{T}, b::AbstractArray) where {T}
+  # Lx = b (8)
+  n, m = size(L) # (8, 6)
+  x = zeros(m) # (6, 1)
+  x[1] = b[1] / L[1, 1]
+
+  for i = 2:m
+    # x[i] = b[i] - sum(L[i, 1:i-1] .* x[1:i-1])
+    x[i] = b[i] 
+    for k = 1:i-1
+      x[i] -= L[i, k] * x[k]
+    end
+    # x[i] /= L[i, i]
   end
   return x
 end
--- test/test-solve.jl
+++ test/test-solve.jl
@@ -1,17 +1,18 @@
 @testset "LU_solve" begin
-  n = 6
+  # n, m = 8, 6 # 非齐次矩阵, LU分解
+  n, m = 6, 6
   p, q = 2, 1
-  A = rand(n, n)
+  A = rand(n, m)
   b = rand(n, 1)
-  force_band!(A, p, q)
+  # force_band!(A, p, q)
+  L, U = LU_full(A)
 
   r = A \ b
-  L, U = LU_full(A)
-  r1 = U \ (L \ b)
-  r1 ≈ r
-
   r2 = solve_U(U, solve_L(L, b))
-  r2 ≈ r
+  @test r2 ≈ r
+
+  r1 = U \ (L \ b)
+  @test r1 ≈ r
 end
 
 @testset "LU_band_solve" begin
@@ -24,7 +25,7 @@ end
 
   B = BandedMat(A, p, q; type="kong", zipped=false)
   BL, BU = LU_band(B)
-  
+
   r2 = solve_U(BU, solve_L(BL, b))
   @test r2 ≈ r
 end
